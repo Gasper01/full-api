f131748294c97e0b26bcca467bb889b86a2ebda2
Gasper1one <wixproyecto2@gmail..com>
2023-05-06 09:17:49 -0600

    congi babel

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-require('./dist');
+import App from './src/app';
+const port = process.env.PORT || 3000;
+
+App.listen(port, () => {
+  console.log(`Server listen on port ${port}`);
+});
--- src/index.js
+++
@@ -1,6 +0,0 @@
-import App from './app';
-const port = process.env.PORT || 3000;
-
-App.listen(port, () => {
-  console.log(`Server listen on port ${port}`);
-});
