84543f47829a9e3e71e5d8c9fdbd7fb952a8a2e9
Gasper1one <wixproyecto2@gmail..com>
2023-05-08 12:49:16 -0600

    change metodo user

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,8 +1,8 @@
-import { Router } from 'express'
-import { signIn, singUp } from '../controller/auth.controller'
-const router = Router()
+import { Router } from 'express';
+import { signIn, singUp } from '../controller/auth.controller';
+const router = Router();
 
-router.post('/singUp', singUp)
-router.get('/signIn', signIn)
+router.post('/singUp', singUp);
+router.post('/signIn', signIn);
 
-export default router
+export default router;
