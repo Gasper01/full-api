16a20dc6314fcca3f62a82c1d3f84665468cefd2
Gasper1one <wixproyecto2@gmail..com>
2023-05-20 23:31:50 -0600

    seacrh

--- src/models/product.model.js
+++ src/models/product.model.js
@@ -1,7 +1,10 @@
 import Joi from 'joi'
 
 const productModel = Joi.object({
-  titulo: Joi.string().min(3).required()
+  producto: Joi.string().min(3).required(),
+  cantidad: Joi.string().min(1).required(),
+  codigo: Joi.string().min(1).required(),
+  unidad:Joi.string().min(1).required()
 })
 
 export default productModel
--- src/routes/products.routes.js
+++ src/routes/products.routes.js
@@ -7,6 +7,7 @@ import { verifyRoles } from '../middlewares/verifyRoles'
 
 const router = Router()
 router.get('/', use.getProducts)
+router.get('/search/:search', use.searchProduct)
 router.get('/:productId', use.getProductsById)
 router.delete('/:productId', verifyToken, verifyRoles, use.deleteProductById)
 router.post('/', verifyToken, verifyRoles, dataValidateHandler(productModel), use.createProduct)
