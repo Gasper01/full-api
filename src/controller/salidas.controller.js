560ec4f8b6c6b1a58abc745a30c4a51e54fe51d8
Gasper1one <wixproyecto2@gmail..com>
2023-06-16 16:32:58 -0600

    salisdas push 35

--- src/controller/salidas.controller.js
+++ src/controller/salidas.controller.js
@@ -1,6 +1,7 @@
 import db from "../db/config.connection";
 let SalidasNoaprovadascache = {};
 let SalidasCacheId = {};
+let SalidasByIdUser = {};
 
 export const createSalidas = async (req, res) => {
   const { fecha, destino, motorista, userId, productos } = req.body;
@@ -15,7 +16,7 @@ export const createSalidas = async (req, res) => {
     };
 
     for (const producto of productos) {
-      const { id, nombre, cantidadAdd, sistema } = producto;
+      const { id, nombre, cantidad, cantidadAdd, sistema } = producto;
 
       const productRef = db.collection("products").doc(id);
       const productSnapshot = await productRef.get();
@@ -47,17 +48,18 @@ export const createSalidas = async (req, res) => {
         newSalidas.productos.push({
           id,
           nombre,
+          cantidad,
           cantidadAdd,
           sistema,
         });
       } else {
-        return res.status(404).json("Producto no encontrado");
+        return res.status(404).json({ message: "Producto no encontrado" });
       }
     }
 
     await db.collection("Salidas").add(newSalidas);
     SalidasNoaprovadascache = {};
-
+    SalidasByIdUser = {};
     return res.status(200).json("ok");
   } catch (error) {
     console.log(error);
@@ -66,7 +68,6 @@ export const createSalidas = async (req, res) => {
       .json({ message: "Se produjo un error inesperado en el servidor" });
   }
 };
-
 export const aprobarSalidas = async (req, res) => {
   const { salidaId } = req.params;
 
@@ -75,7 +76,7 @@ export const aprobarSalidas = async (req, res) => {
     const salidaSnapshot = await salidaRef.get();
 
     if (!salidaSnapshot.exists) {
-      return res.status(404).json("Salida no encontrada");
+      return res.status(404).json({ message: "Salida no encontrada" });
     }
 
     const salidaData = salidaSnapshot.data();
@@ -92,11 +93,31 @@ export const aprobarSalidas = async (req, res) => {
         const productData = productSnapshot.data();
         const nuevaCantidad = productData.cantidad - cantidadAdd;
 
+        let sumacantidad = 0;
+
+        // Suma todas las cantidades adicionales para los productos con el mismo ID
+        for (const prod of productos) {
+          if (prod.id === id) {
+            sumacantidad += prod.cantidadAdd;
+          }
+        }
+
+        // Comprueba si la suma de las cantidades añadidas supera la cantidad existente
+        if (sumacantidad > productData.cantidad) {
+          return res.status(400).json({
+            message:
+              "Supera la cantidad existente  " +
+              "  Nombre: " +
+              productData.nombre +
+              "  Existente: " +
+              productData.cantidad,
+          });
+        }
         await productRef.update({
           cantidad: nuevaCantidad,
         });
       } else {
-        return res.status(404).json("Producto no encontrado");
+        return res.status(404).json({ message: "Producto no encontrado" });
       }
     }
 
@@ -104,15 +125,18 @@ export const aprobarSalidas = async (req, res) => {
     await salidaRef.update({
       aprobada: true,
     });
+    SalidasNoaprovadascache = {};
+    SalidasByIdUser = {};
 
-    return res.status(200).json("Salida aprobada y cantidades actualizadas");
+    return res
+      .status(200)
+      .json({ message: "Salida aprobada y cantidades actualizadas" });
   } catch (error) {
     return res
       .status(500)
       .json({ message: "Se produjo un error inesperado en el servidor" });
   }
 };
-
 export const getSalidasNoaprovadas = async (req, res) => {
   try {
     // Verificar si los resultados están en la caché
@@ -174,7 +198,6 @@ export const getSalidasNoaprovadas = async (req, res) => {
       .json({ message: "Se produjo un error inesperado en el servidor" });
   }
 };
-
 export const getSalidasById = async (req, res) => {
   const { salidaId } = req.params;
   try {
@@ -187,7 +210,7 @@ export const getSalidasById = async (req, res) => {
     const doc = await Salidas.get();
 
     if (!doc.exists) {
-      return res.status(403).json(`Salida not found with id ${salidaId}`);
+      return res.status(403).json({ message: `Salida not found with id ` });
     }
 
     const userDataPromise = db.collection("users").doc(doc.data().userId).get();
@@ -202,8 +225,10 @@ export const getSalidasById = async (req, res) => {
 
     const response = {
       id: doc.id,
-      fecha: doc.data().fecha,
+      aprobada: doc.data().aprobada,
       destino: doc.data().destino,
+      fecha: doc.data().fecha,
+      motorista: doc.data().motorista,
       user: userData,
       productos: doc.data().productos,
     };
@@ -212,6 +237,140 @@ export const getSalidasById = async (req, res) => {
     SalidasCacheId[salidaId] = response;
 
     return res.status(200).json(response);
+  } catch (error) {
+    return res
+      .status(500)
+      .json({ message: "An unexpected error occurred on the server" });
+  }
+};
+export const getSalidasByIdUser = async (req, res) => {
+  const { Iduser, page } = req.params;
+  let limit = 10;
+
+  // Verificar si la respuesta está en la caché
+  if (SalidasByIdUser[Iduser]) {
+    const startIndex = (page - 1) * limit;
+    const endIndex = page * limit;
+    const salidas = SalidasByIdUser[Iduser].slice(startIndex, endIndex);
+
+    const totalPages = Math.ceil(SalidasByIdUser[Iduser].length / limit);
+
+    res.status(200).json({ salidas, totalPages });
+    return;
+  }
+
+  const Salidas = db.collection("Salidas");
+  let query = Salidas.where("userId", "==", Iduser);
+
+  try {
+    const snapshot = await query.get();
+    const salidas = [];
+
+    snapshot.forEach((doc) => {
+      salidas.push({ id: doc.id, ...doc.data() });
+    });
+
+    // Ordenar las salidas por fecha ascendente
+
+    salidas.sort((a, b) => b.fecha.localeCompare(a.fecha));
+
+    // Guardar la respuesta en la caché
+    SalidasByIdUser[Iduser] = salidas;
+
+    const startIndex = (page - 1) * limit;
+    const endIndex = page * limit;
+    const paginatedSalidas = salidas.slice(startIndex, endIndex);
+    const totalPages = Math.ceil(salidas.length / limit);
+
+    res.status(200).json({ salidas: paginatedSalidas, totalPages });
+  } catch (error) {
+    res.status(500).json({ message: "Error getting salidas by User ID" });
+  }
+};
+export const updateSalidasById = async (req, res) => {
+  const { salidaId } = req.params; // Obtén el ID de la salida de la solicitud
+
+  try {
+    const { productos } = req.body;
+
+    // Obtén la salida existente de la base de datos
+    const salidaRef = db.collection("Salidas").doc(salidaId);
+    const salidaDoc = await salidaRef.get();
+
+    if (!salidaDoc.exists) {
+      return res.status(403).json({ message: "Salida not found with id" });
+    }
+
+    const updataSalidas = salidaDoc.data();
+    const updatedProductos = [];
+
+    // Realiza las comprobaciones para los productos actualizados
+    for (const producto of productos) {
+      const { id, nombre, cantidad, cantidadAdd, sistema } = producto;
+
+      const productRef = db.collection("products").doc(id);
+      const productSnapshot = await productRef.get();
+
+      if (productSnapshot.exists) {
+        const productData = productSnapshot.data();
+
+        // Suma todas las cantidades adicionales para los productos con el mismo ID
+        let sumacantidad = 0;
+        for (const prod of productos) {
+          if (prod.id === id) {
+            sumacantidad += prod.cantidadAdd;
+          }
+        }
+
+        // Comprueba si la suma de las cantidades añadidas supera la cantidad existente
+        if (sumacantidad > productData.cantidad) {
+          return res.status(400).json({
+            message:
+              "Supera la cantidad existente. Nombre: " +
+              productData.nombre +
+              ", Existente: " +
+              productData.cantidad,
+          });
+        }
+
+        // Agrega el producto al array de productos de la salida
+        updatedProductos.push({
+          id,
+          nombre,
+          cantidad,
+          cantidadAdd,
+          sistema,
+        });
+      } else {
+        return res.status(404).json({ message: "Producto no encontrado" });
+      }
+    }
+
+    updataSalidas.productos = updatedProductos;
+    await salidaRef.update(updataSalidas);
+    SalidasByIdUser = {};
+    SalidasCacheId = {};
+    return res
+      .status(200)
+      .json({ message: "Salida actualizada correctamente" });
+  } catch (error) {
+    return res.status(500).json({ message: "Error al actualizar la salida" });
+  }
+};
+export const DeleteSalidasById = async (req, res) => {
+  try {
+    const Salidas = db.collection("Salidas").doc(req.params.salidaId);
+    const SalidaId = await Salidas.get();
+
+    if (!SalidaId.exists) {
+      return res.status(404).json({ message: "No Salida found with id" });
+    }
+
+    await Salidas.delete();
+    SalidasByIdUser = {};
+    SalidasCacheId = {};
+    SalidasNoaprovadascache = {};
+    return res.status(200).json({ message: "ok" });
   } catch (error) {
     return res
       .status(500)
--- src/routes/salidas.routes.js
+++ src/routes/salidas.routes.js
@@ -3,16 +3,22 @@ import { verifyToken } from "../middlewares/verifyJwt";
 import dataValidateHandler from "../middlewares/dataValidateHandler";
 import salidasModel from "../models/salidas.model";
 import {
+  DeleteSalidasById,
   aprobarSalidas,
   createSalidas,
   getSalidasById,
+  getSalidasByIdUser,
   getSalidasNoaprovadas,
+  updateSalidasById,
 } from "../controller/salidas.controller";
 
 const router = Router();
 
 router.get("/", getSalidasNoaprovadas);
 router.get("/:salidaId", getSalidasById);
+router.get("/user/:Iduser/:page", getSalidasByIdUser);
 router.post("/", verifyToken, createSalidas);
 router.post("/aprobar/:salidaId", verifyToken, aprobarSalidas);
+router.put("/:salidaId", updateSalidasById);
+router.delete("/:salidaId", DeleteSalidasById);
 export default router;
