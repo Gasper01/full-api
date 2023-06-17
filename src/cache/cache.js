2cec14cc4c86f35208f86399d13d338b773d658b
Gasper1one <wixproyecto2@gmail..com>
2023-06-17 14:20:36 -0600

    retore files no cache

--- src/cache/cache.js
+++ src/cache/cache.js
@@ -1,15 +1,7 @@
-import NodeCache from "node-cache";
-
 export let productsCache = {};
 export let productSearchCache = {};
 
 export const clearCache = () => {
   productsCache = {};
   productSearchCache = {};
-};
-
-export const cache = new NodeCache();
-
-export const clearCacheNode = () => {
-  cache.flushAll();
 };
