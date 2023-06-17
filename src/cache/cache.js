ede1adb6c6415ca4659055c51019ec3da8bc4a6b
Gasper1one <wixproyecto2@gmail..com>
2023-06-17 14:14:57 -0600

    cache 2

--- src/cache/cache.js
+++ src/cache/cache.js
@@ -1,2 +1,15 @@
+import NodeCache from "node-cache";
+
 export let productsCache = {};
 export let productSearchCache = {};
+
+export const clearCache = () => {
+  productsCache = {};
+  productSearchCache = {};
+};
+
+export const cache = new NodeCache();
+
+export const clearCacheNode = () => {
+  cache.flushAll();
+};
