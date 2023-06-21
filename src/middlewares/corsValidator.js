21a4650de805c7583c5c0f9337315db4b313abfa
Gasper1one <wixproyecto2@gmail..com>
2023-06-21 14:56:24 -0600

    origen

--- src/middlewares/corsValidator.js
+++ src/middlewares/corsValidator.js
@@ -1,16 +1,22 @@
-import cors from 'cors';
+import cors from "cors";
 
-const allowedOrigins = ['http://localhost:3000', 'https://cib-web.vercel.app'];
+const allowedOrigins = [
+  "http://localhost:3000",
+  "https://cib-web.vercel.app/",
+  "https://cib-web.vercel.app",
+  "https://cib-9b1txbxro-gasper.vercel.app/",
+  "cib-9b1txbxro-gasper.vercel.app",
+];
 
 const corsOptions = {
   origin: (origin, callback) => {
     if (allowedOrigins.includes(origin)) {
       callback(null, true);
     } else {
-      callback('Not allowed by CORS');
+      callback("Not allowed by CORS");
     }
   },
-  credentials: true
+  credentials: true,
 };
 
 export default cors(corsOptions);
